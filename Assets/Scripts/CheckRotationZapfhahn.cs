5bbe8617be15a1e0fdf3ab9b3394d1e36e7a4df9
BlauBierBube <jonas.wunner@gmail.com>
2023-06-15 15:44:42 +0200

    Update Zapfhahn

--- Assets/Scripts/CheckRotationZapfhahn.cs
+++ Assets/Scripts/CheckRotationZapfhahn.cs
@@ -14,55 +14,119 @@ public class CheckRotationZapfhahn : MonoBehaviour
     public float TagetRange = 10;
 
     public bool AlwaysAktive = true;
+    public bool notGrabbed = false;
     private bool isActive = false;
     public UnityEvent onSolved;
     public UnityEvent stop;
     private float startRot;
 
+    private bool rotBack = false;
+    private Quaternion startRotation;
+    public Quaternion targetRotation;
+    public float rotationDuration = 2.0f; // Adjust the duration as per your requirement
+    private float rotationTimer = 0.0f;
 
     private void Start()
     {
+        startRotation = transform.rotation;
         startRot = transform.localEulerAngles.x;
+
     }
     void Update()
     {
 
-        if(isActive != true && AlwaysAktive==true || gameObject.GetComponent<Grabbable>()._activeTransformer != null) // If GameObject is Grabbed
+        //Debug.LogError(currentRotation.x);
+        if (AlwaysAktive==true || gameObject.GetComponent<Grabbable>()._activeTransformer != null) // If GameObject is Grabbed
         {
             //Zielbereich +-Range/2 fuer Tolleranz
-            if (currentRotation.x <= (TagetArea + (TagetRange / 2)) && currentRotation.x >= (TagetArea - (TagetRange / 2)) || currentRotation.x == TagetArea)
+            if (currentRotation.x <= (TagetArea + (TagetRange / 2)) && currentRotation.x >= (TagetArea - (TagetRange / 2)) && isActive == false || currentRotation.x == TagetArea)
             {
-                StartCoroutine(WaitSec());
+                //Debug.LogError("in if");
+                Solved();
+                //StartCoroutine(WaitSec());
             }
-            else
+            //Zielbereich +-Range/2 fuer Tolleranz
+            if (currentRotation.x <= (TagetArea - (TagetRange / 2)) && isActive==true || currentRotation.x == startRot)
             {
-                StopAllCoroutines();
+                //Debug.LogError("in Else");
+                Stop();
+                //StartCoroutine(WaitSec());
             }
 
-        }if(startRot == currentRotation.x && isActive == true)
-        {
-            Stop();
         }
-        
+
+
+
+        /*
+        if (AlwaysAktive == false)// || gameObject.GetComponent<Grabbable>()._activeTransformer == null)
+        {
+            StartCoroutine(RotateBack());
+            if (currentRotation.x == startRot)
+                rotBack = false;
+        }
+        */
+
+        //currentRotation.x <= (TagetArea - (TagetRange / 2)) || currentRotation.x == startRot
+        if (currentRotation != transform.localEulerAngles)
+        {
+            currentRotation = transform.localEulerAngles;
+        }
+
+        if (currentRotation.x != startRotation.x)
+        {
+            if (notGrabbed==true && AlwaysAktive == true)// || gameObject.GetComponent<Grabbable>()._activeTransformer == null)
+            {
+                if (rotBack == false)
+                {
+                    RotateBack();
+                }
+                //Debug.LogError("RotateBack");
+                rotationTimer += Time.deltaTime;
+                float t = rotationTimer / rotationDuration;
+                transform.rotation = Quaternion.Slerp(targetRotation, startRotation, t);
+            }
+            //Debug.LogError("RotateBack");
+        }
+        if (currentRotation.x == startRotation.x)
+        {
+            rotBack = false;
+        }
+    }
+    
+    void RotateBack()
+    {
+        rotBack = true;
+        rotationTimer = 0.0f;
+        targetRotation = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z);
     }
 
     IEnumerator WaitSec()
     {
-        yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(0.1f);
         Solved();
     }
 
     private void Solved()
     {
         StopAllCoroutines();
-        isActive = true;
-        //Textfield.text = "is Solved";
-        onSolved.Invoke();
+
+
+        if (isActive==false)
+        {
+            //Debug.LogError("Solved");
+            isActive = true;
+            onSolved.Invoke();
+        }
+
     }
     private void Stop()
     {
         StopAllCoroutines();
-        isActive = false;
-        stop.Invoke();
+        if (isActive==true)
+        {
+            //Debug.LogError("Stop");
+            isActive = false;
+            stop.Invoke();
+        }
     }
 }
